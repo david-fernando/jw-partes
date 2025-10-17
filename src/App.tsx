d76da2d71f24c2c4cc9aaceed990efe4d2891c85
david-fernando <davidfernandodamata21@gmail.com>
2025-10-17 10:59:01 -0300

    Add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, Image } from 'react-native';
+import BackupButton from './components/BackupButton';
+import Tabs from './components/Tabs';
+
+import styles from './styles/App.style';
+
+import spotlight from './images/spotlight.png';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <BackupButton />
+      <View style={styles.content}>
+        <Image source={spotlight} style={styles.image} />
+        <Text style={styles.text}>Ainda não há nada aqui!</Text>
+        <Text style={styles.instruction}>Comece a fazer designações ou restaure um backup</Text>
+      </View>
+      <Tabs />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
