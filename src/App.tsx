7366b31769acfec47e751f6eb7b0d2f80c44c94f
david-fernando <davidfernandodamata21@gmail.com>
2025-10-20 20:12:54 -0300

    Wrap App component with SearchProvider for context management

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, Image } from 'react-native';
+import { SearchProvider } from './context/SearchContext'
 import BackupButton from './components/BackupButton';
 import Tabs from './components/Tabs';
 
@@ -9,15 +10,17 @@ import spotlight from './images/spotlight.png';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <BackupButton />
-      <View style={styles.content}>
-        <Image source={spotlight} style={styles.image} />
-        <Text style={styles.text}>Ainda não há nada aqui!</Text>
-        <Text style={styles.instruction}>Comece a fazer designações ou restaure um backup</Text>
+    <SearchProvider>
+      <View style={styles.container}>
+        <BackupButton />
+        <View style={styles.content}>
+          <Image source={spotlight} style={styles.image} />
+          <Text style={styles.text}>Ainda não há nada aqui!</Text>
+          <Text style={styles.instruction}>Comece a fazer designações ou restaure um backup</Text>
+        </View>
+        <Tabs />
+        <StatusBar style="auto" />
       </View>
-      <Tabs />
-      <StatusBar style="auto" />
-    </View>
+    </SearchProvider>
   );
 }
