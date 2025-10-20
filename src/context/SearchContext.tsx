bdcb5aa9d9601f475eeed107d6220148a123df32
david-fernando <davidfernandodamata21@gmail.com>
2025-10-20 20:12:41 -0300

    Add SearchContext

---
+++ src/context/SearchContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, useState, useRef, useEffect, use } from 'react'
+import { TextInput } from 'react-native';
+import { Search, ContextChildren } from '../interface/interface'
+
+const SearchContext = createContext<Search>({
+  isSearchOpen: false,
+  setIsSearchOpen: () => {},
+  actionButtonWidth: 49,
+  gap: 23,
+})
+
+function SearchProvider({ children }: ContextChildren) {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const actionButtonWidth = 49;
+  const gap = 23;
+  
+  const contextValue = {
+    isSearchOpen,
+    setIsSearchOpen,
+    actionButtonWidth,
+    gap,
+  }
+
+  return (
+    <SearchContext.Provider value={contextValue}>
+      {children}
+    </SearchContext.Provider>
+  )
+}
+
+export {
+  SearchContext,
+  SearchProvider
+}
