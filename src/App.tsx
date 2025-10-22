2ae060d5ea2a781437c7c11dfbc501ef1ce40822
david-fernando <davidfernandodamata21@gmail.com>
2025-10-22 01:03:26 -0300

    Add Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, Image } from 'react-native';
+import { useContext } from 'react';
 import { SearchProvider } from './context/SearchContext'
-import BackupButton from './components/BackupButton';
-import Tabs from './components/Tabs';
-
-import styles from './styles/App.style';
-
-import spotlight from './images/spotlight.png';
+import Main from './Main';
 
 export default function App() {
   return (
     <SearchProvider>
-      <View style={styles.container}>
-        <BackupButton />
-        <View style={styles.content}>
-          <Image source={spotlight} style={styles.image} />
-          <Text style={styles.text}>Ainda não há nada aqui!</Text>
-          <Text style={styles.instruction}>Comece a fazer designações ou restaure um backup</Text>
-        </View>
-        <Tabs />
-        <StatusBar style="auto" />
-      </View>
+      <Main />
     </SearchProvider>
   );
 }
